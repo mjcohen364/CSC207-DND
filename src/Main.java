b16c1faa83318eb8cfa4caa3a339e73e4ff82865
Alice Sedgwick <alicejsedgwick2004@gmail.com>
2023-09-29 17:42:28 -0400

    Messed around with main class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        curl --location 'https://www.dnd5eapi.co/api/ability-scores/cha'
+    }
+}
