4dac2a35e10fc9a79719c9940b4c8ec09c2fde2b
mjcohen364 <matthew.cohen@mail.utoronto.ca>
2023-10-30 12:16:53 -0400

    Added methods to Character Interface, attributes to Player Class, and created DndClass Class

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,30 @@
+import java.awt.dnd.DnDConstants;
+
 public interface Character {
-//    Test
+    String getName();
+    List<Attribute> getAttributes();
+    Inventory getInventory();
+    HitDice getHitDice();
+    ArmourClass getArmourClass();
+    Size getSize();
+    ClassLevel getClassLevel();
+    Race getRace();
+    DndClass getDndClass();
+    Background getBackground();
+    List<Proficiency> getSkillProficiencies();
+    List<Proficiency> getEquipmentProficiencies();
+    List<Spell> getSpellsKnown();
+    void setName();
+    void addAttribute();
+    void addItem();
+    void setHitDice();
+    void setArmourClass();
+    void setSize();
+    void setClassLevel();
+    void setRace();
+    void setDndClass();
+    void setBackground();
+    void addSkillProficiency();
+    void addEquipmentProficiency();
+    void addSpell();
 }
---
+++ src/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    String name;
+    List<Attribute> attributes;
+    Inventory inventory;
+    HitDice hitDice;
+    ArmourClass armourClass;
+    Size size;
+    ClassLevel classLevel;
+    Race race;
+    DndClass dndClass;
+    Background background;
+    List<Proficiency> skillProficiencies;
+    List<Proficiency> equipmentProficiencies;
+    List<Spell> spellsKnown;
+}
