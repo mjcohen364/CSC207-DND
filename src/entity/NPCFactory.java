4aa55108d1b0d3819a85ac1f0f4d95abc81bd3e2
Alice Sedgwick <alicejsedgwick2004@gmail.com>
2023-10-30 12:47:14 -0400

    missed a couple classes in the last commit

--- src/entity/NPCFactory.java
+++ src/entity/NPCFactory.java
@@ -1,2 +1,14 @@
-package entity;public class NPCFactory {
+package entity;
+
+public class NPCFactory {
+    /**
+     * Requires: password is valid.
+     * @param name
+     * @return Character
+     */
+
+    @Override
+    public Character create(String name) {
+        return new Player(name);
+    }
 }
--- src/use_case/character_name/CharacterNameInputData.java
+++ src/use_case/character_name/CharacterNameInputData.java
@@ -1,26 +1,14 @@
 package use_case.character_name;
 
-public class SignupInputData {
+public class CharacterNameInputData {
 
-    final private String username;
-    final private String password;
-    final private String repeatPassword;
+    final private String name;
 
-    public SignupInputData(String username, String password, String repeatPassword) {
-        this.username = username;
-        this.password = password;
-        this.repeatPassword = repeatPassword;
+    public CharacterNameInputData(String name) {
+        this.name = name;
     }
 
-    String getUsername() {
-        return username;
-    }
-
-    String getPassword() {
-        return password;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
+    String getName() {
+        return name;
     }
 }
