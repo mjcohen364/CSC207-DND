a54f1b76fdee0f216fca22132dbdd969a8a05b3d
mjcohen364 <matthew.cohen@mail.utoronto.ca>
2023-10-30 12:21:27 -0400

    made Player implement Character, added getters to Player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,4 @@
-public class Player {
+public class Player implements Character{
     String name;
     List<Attribute> attributes;
     Inventory inventory;
@@ -12,4 +12,69 @@ public class Player {
     List<Proficiency> skillProficiencies;
     List<Proficiency> equipmentProficiencies;
     List<Spell> spellsKnown;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    @Override
+    public HitDice getHitDice() {
+        return hitDice;
+    }
+
+    @Override
+    public ArmourClass getArmourClass() {
+        return armourClass;
+    }
+
+    @Override
+    public Size getSize() {
+        return size;
+    }
+
+    @Override
+    public ClassLevel getClassLevel() {
+        return classLevel;
+    }
+
+    @Override
+    public Race getRace() {
+        return race;
+    }
+
+    @Override
+    public DndClass getDndClass() {
+        return dndClass;
+    }
+
+    @Override
+    public Background getBackground() {
+        return background;
+    }
+
+    @Override
+    public List<Proficiency> getSkillProficiencies() {
+        return skillProficiencies;
+    }
+
+    @Override
+    public List<Proficiency> getEquipmentProficiencies() {
+        return equipmentProficiencies;
+    }
+
+    @Override
+    public List<Spell> getSpellsKnown() {
+        return spellsKnown;
+    }
 }
