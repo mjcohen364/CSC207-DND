52b30b585541d1a8aa780f9e2c8a13ef6092dc48
Jaewon Lee <justjae.lee@mail.utoronto.ca>
2023-11-18 13:52:11 -0500

    Implemented Attributes(Class), Proficiency(Class) and Size(Enum)

---
+++ src/entity/DndAttribute.java
@@ -0,0 +1,16 @@
+package entity;
+
+public class DndAttribute {
+    String name;
+    int value;
+    int modifier;
+    public void init(String name, int value){
+        this.name = name;
+        this.value = value;
+        this.modifier = (int) ((value-10)/2);
+    }
+    public void update(int value){
+        this.value = value;
+        this.modifier = (int) ((value-10)/2);
+    }
+}
